19c5ab1c0d88d1292d6f018b94718fac3b060b00
Nuur-R <firdausnuurr@gmail.com>
2024-04-27 21:29:45 +0700

    add 2 handed function

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ mp_drawing = mp.solutions.drawing_utils
 def detect_hand_gesture(image):
     # Mendeteksi tangan dalam gambar
     results = hands.process(image)
+    # Inisialisasi variabel untuk menyimpan koordinat bounding box
+    bounding_boxes = []
     # Jika tangan terdeteksi
     if results.multi_hand_landmarks:
         for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
@@ -19,6 +21,7 @@ def detect_hand_gesture(image):
             # Gambar bounding box
             x_min, y_min, x_max, y_max = calculate_bounding_box(hand_landmarks, image.shape[1], image.shape[0])
             cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+            bounding_boxes.append((x_min, y_min, x_max, y_max))
 
             # Tambahkan label
             label = "Kanan" if is_right_hand else "Kiri"
@@ -26,7 +29,6 @@ def detect_hand_gesture(image):
             
             # Gambar skeleton tangan
             mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-
             # Crop gambar sesuai dengan bounding box
             cropped_image = crop_image(image, x_min, y_min, x_max, y_max)
             # Tampilkan gambar crop sesuai dengan tangan kanan atau kiri
@@ -35,6 +37,11 @@ def detect_hand_gesture(image):
                     cv2.imshow("Tangan kanan", cropped_image)
                 else:
                     cv2.imshow("Tangan kiri", cropped_image)
+    # Crop gambar sesuai dengan bounding box tangan
+    if len(bounding_boxes) == 2:
+        cropped_image = crop_double_handed(image, bounding_boxes[0], bounding_boxes[1])
+        if cropped_image is not None:
+            cv2.imshow("Double Handed", cropped_image)
 
     return image
 
@@ -50,11 +57,21 @@ def calculate_bounding_box(hand_landmarks, image_width, image_height):
         y_max = max(y_max, y)
     return x_min, y_min, x_max, y_max
 
-# Fungsi untuk memotong gambar sesuai dengan bounding box
 def crop_image(image, x_min, y_min, x_max, y_max):
     # Periksa ukuran gambar sebelum memotong
     if x_min >= 0 and y_min >= 0 and x_max <= image.shape[1] and y_max <= image.shape[0]:
         cropped_image = image[y_min:y_max, x_min:x_max]
+        return cropped_image
+    else:
+        return None
+# Fungsi untuk memotong gambar sesuai dengan bounding box tangan
+def crop_double_handed(image, bounding_box1, bounding_box2):
+    x1_min, y1_min, x1_max, y1_max = bounding_box1
+    x2_min, y2_min, x2_max, y2_max = bounding_box2
+    # Periksa apakah koordinat bounding box valid
+    if x1_min >= 0 and y1_min >= 0 and x1_max <= image.shape[1] and y1_max <= image.shape[0] \
+            and x2_min >= 0 and y2_min >= 0 and x2_max <= image.shape[1] and y2_max <= image.shape[0]:
+        cropped_image = image[min(y1_min, y2_min):max(y1_max, y2_max), min(x1_min, x2_min):max(x1_max, x2_max)]
         return cropped_image
     else:
         return None
